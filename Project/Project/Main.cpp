8ffa8353ab29732c463f41d2bc95235fe1ad4ccc
Rupam Debnath <44625252+rupamdebnath@users.noreply.github.com>
2022-04-04 13:40:30 +0530

    Added EndGame() function to check for win conditions and stop game if any conditions are met, horizontal check, vertical check and diagonal check of either red or blue at any turn

--- Project/Project/Main.cpp
+++ Project/Project/Main.cpp
@@ -44,6 +44,10 @@ void set()
             currentBoard[i][j] = 0;            
         }
     }
+    for (int i = 0; i < WIDTH; i++)
+    {
+        columnMax[i] = 0;
+    }
 }
 
 void PlayRedTurn(int a)
@@ -76,7 +80,7 @@ void PlayBlueTurn(int a)
         { 
             if (currentBoard[i][a - 1] == 0)
             {
-                currentBoard[i][a - 1] = 1;
+                currentBoard[i][a - 1] = 2;
                 blue[bluecount].setPosition(((a - 1) * 100), (i * 100));
                 bluecount++;
                 redTurn = true;
@@ -86,6 +90,97 @@ void PlayBlueTurn(int a)
     }
     else
         redTurn = false;
+}
+
+bool EndGame()
+{
+    //check horizontal match of 4
+    for (int i = 0; i < HEIGHT; i++)
+    {
+        for (int j = 0; j < WIDTH - 3; j++)
+        {
+            if (currentBoard[i][j] == 1 && currentBoard[i][j + 1] == 1 && currentBoard[i][j + 2] == 1 && currentBoard[i][j + 3] == 1)
+            {
+                cout << "Red has won!\n";
+                //system("pause");
+                return true;
+            }
+            else if (currentBoard[i][j] == 2 && currentBoard[i][j + 1] == 2 && currentBoard[i][j + 2] == 2 && currentBoard[i][j + 3] == 2)
+            {
+                cout << "Blue has won!\n";
+                //system("pause");
+                return true;
+            }
+        }
+    }
+
+    //check vertical match of 4
+    for (int i = 0; i < HEIGHT - 3; i++)
+    {
+        for (int j = 0; j < WIDTH; j++)
+        {
+            if (currentBoard[i][j] == 1 && currentBoard[i + 1][j] == 1 && currentBoard[i + 2][j] == 1 && currentBoard[i + 3][j] == 1)
+            {
+                cout << "Red has won!\n";
+                //system("pause");
+                return true;
+            }
+            else if (currentBoard[i][j] == 2 && currentBoard[i + 1][j] == 2 && currentBoard[i + 2][j] == 2 && currentBoard[i + 3][j] == 2)
+            {
+                cout << "Blue has won!\n";
+                //system("pause");
+                return true;
+            }
+        }
+    }
+
+    //check diagonal match of 4
+    for (int i = 0; i < HEIGHT - 3; i++)
+    {
+        for (int j = 0; j < WIDTH - 3; j++)
+        {
+            if (currentBoard[i][j] == 1 && currentBoard[i + 1][j + 1] == 1 && currentBoard[i + 2][j + 2] == 1 && currentBoard[i + 3][j + 3] == 1)
+            {
+                cout << "Red has won!\n";
+                system("pause");
+                return true;
+            }
+            else if (currentBoard[i][j] == 2 && currentBoard[i + 1][j + 1] == 2 && currentBoard[i + 2][j + 2] == 2 && currentBoard[i + 3][j + 3] == 2)
+            {
+                cout << "Blue has won!\n";
+                system("pause");
+                return true;
+            }
+        }
+    }
+
+    for (int i = 0; i < HEIGHT - 3; i++)
+    {
+        for (int j = WIDTH - 1; j >= 3; j--)
+        {
+            if (currentBoard[i][j] == 1 && currentBoard[i + 1][j - 1] == 1 && currentBoard[i + 2][j - 2] == 1 && currentBoard[i + 3][j - 3] == 1)
+            {
+                cout << "Red has won!\n";
+                //system("pause");
+                return true;
+            }
+            else if (currentBoard[i][j] == 2 && currentBoard[i + 1][j - 1] == 2 && currentBoard[i + 2][j - 2] == 2 && currentBoard[i + 3][j - 3] == 2)
+            {
+                cout << "Blue has won!\n";
+                //system("pause");
+                return true;
+            }
+        }
+    }
+
+    //check if noone has got a match, hence match tied
+    if (turns == 42)
+    {
+        cout << "Draw game between Red and Blue, its a Tie" << endl;
+        system("pause");
+        return true;
+    }        
+    return false;
 }
 
 int main()
@@ -137,7 +232,11 @@ int main()
             window.draw(blue[i]);
         }
         window.display();
-        window.display();
+        if (EndGame())
+        {
+            system("pause");
+            set();
+        }
     }
 
     return 0;
